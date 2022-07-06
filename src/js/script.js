81e062bc10708fd5342a338e592cc7bf95215db9
max_air <max_air@bk.ru>
2022-07-06 11:51:25 +0300

    Подключение шрифтов

---
+++ src/js/script.js
@@ -0,0 +1,13 @@
+"use strict";
+
+let slider = tns({
+    container: '.courusel__inner',
+    controls: false,
+    items: 1,
+    slideBy: 'page',
+    autoplay: true,
+    autoplayButtonOutput: false,
+    speed: 900,
+    navPosition: "bottom",
+    animateIn: "tns-fadeIn"
+});
